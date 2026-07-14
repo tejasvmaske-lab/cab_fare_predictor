31b1901b1c2007229c63860af17208790c5ee5fc
tejasvmaske-lab <tejas.v.maske@gmail.com>
2026-07-14 23:30:34 +0530

    changes

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -8,15 +8,15 @@ function About() {
                 <section className="about-heading">
                     <h1>About the Model</h1>
                     <ScrollFloat
-                    animationDuration={1}
+                    animationDuration={5}
                     ease='back.inOut(2)'
                     scrollStart='top bottom-=10%'
                     scrollEnd='bottom 80%'
                     stagger={0.03}
                     >
-                    Built using Linear Regression and trained on over 193,000 real-world cab trips 
-                    collected from New York City. The model estimates fares using trip distance as
-                    its single input feature.
+                        Built using Linear Regression and trained on over 193,000 real-world cab trips 
+                        collected from New York City. The model estimates fares using trip distance as
+                        its single input feature.
                     </ScrollFloat>
                 </section>
             </section>
