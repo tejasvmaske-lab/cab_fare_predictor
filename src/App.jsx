2d47cf59f558dd3033f4ddbabecec655a0431099
tejasvmaske-lab <tejas.v.maske@gmail.com>
2026-07-14 22:37:49 +0530

    Scroll float for about text

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Hero from './sections/Hero.jsx';
+import About from './sections/About.jsx';
 import './App.css'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Hero />
+      <About />
     </>
   )
 }
---
+++ src/sections/About.jsx
@@ -0,0 +1,27 @@
+import ScrollFloat from '../components/ScrollFloat/ScrollFloat.jsx';
+import './About.css';
+
+function About() {
+    return (
+        <>
+            <section className="about">
+                <section className="about-heading">
+                    <h1>About the Model</h1>
+                    <ScrollFloat
+                    animationDuration={1}
+                    ease='back.inOut(2)'
+                    scrollStart='top bottom-=10%'
+                    scrollEnd='bottom 80%'
+                    stagger={0.03}
+                    >
+                    Built using Linear Regression and trained on over 193,000 real-world cab trips 
+                    collected from New York City. The model estimates fares using trip distance as
+                    its single input feature.
+                    </ScrollFloat>
+                </section>
+            </section>
+        </>
+    )
+}
+
+export default About;
