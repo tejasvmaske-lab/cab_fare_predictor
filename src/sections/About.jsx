067b34c2daf69f8e1e7b0fc25a7e416064080fe1
tejasvmaske-lab <tejas.v.maske@gmail.com>
2026-07-15 19:27:47 +0530

    alignment issues solved for magic bento

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,4 +1,5 @@
 import ScrollFloat from '../components/ScrollFloat/ScrollFloat.jsx';
+import MagicBento from '../components/MagicBento/MagicBento.jsx';
 import './About.css';
 
 function About() {
@@ -8,16 +9,29 @@ function About() {
                 <section className="about-heading">
                     <h1>About the Model</h1>
                     <ScrollFloat
-                    animationDuration={5}
-                    ease='back.inOut(2)'
-                    scrollStart='top bottom-=10%'
-                    scrollEnd='bottom 80%'
-                    stagger={0.03}
+                        animationDuration={5}
+                        ease='back.inOut(2)'
+                        scrollStart='top bottom-=10%'
+                        scrollEnd='bottom 80%'
+                        stagger={0.03}
                     >
                         Built using Linear Regression and trained on over 193,000 real-world cab trips 
                         collected from New York City. The model estimates fares using trip distance as
                         its single input feature.
                     </ScrollFloat>
+                    <MagicBento 
+                        textAutoHide={true}
+                        enableStars
+                        enableSpotlight
+                        enableBorderGlow={true}
+                        enableTilt={false}
+                        enableMagnetism={false}
+                        clickEffect
+                        spotlightRadius={400}
+                        particleCount={12}
+                        glowColor="132, 0, 255"
+                        disableAnimations={false}
+                    />
                 </section>
             </section>
         </>
